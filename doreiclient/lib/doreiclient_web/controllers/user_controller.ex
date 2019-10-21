bd3d5f0c0183b10e0e447d73083507649d868a70
ohzw <52651644+ohzw@users.noreply.github.com>
2019-10-21 18:16:44 +0900

    ログイン認証

--- doreiclient/lib/doreiclient_web/controllers/user_controller.ex
+++ doreiclient/lib/doreiclient_web/controllers/user_controller.ex
@@ -3,6 +3,7 @@ defmodule DoreiclientWeb.UserController do
 
   alias Doreiclient.Accounts
   alias Doreiclient.Accounts.User
+  alias Doreiclient.Accounts.Guardian
 
   action_fallback DoreiclientWeb.FallbackController
 
@@ -26,19 +27,33 @@ defmodule DoreiclientWeb.UserController do
   end
 
   def update(conn, %{"id" => id, "user" => user_params}) do
+    current_user = Accounts.current_user(conn)
     user = Accounts.get_user!(id)
-
-    with {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
+    if current_user.idd == user.id do
+      Accounts.update_user(user, user_params)
       render(conn, "show.json", user: user)
+    else
+      conn
+      |> json%{message: "auth error."}
     end
   end
 
   def delete(conn, %{"id" => id}) do
+    current_user = Accounts.current_user(conn)
     user = Accounts.get_user!(id)
-
-    with {:ok, %User{}} <- Accounts.delete_user(user) do
-      send_resp(conn, :no_content, "")
+    if current_user.id == user.id do
+      Accounts.delete_user(user)
+      conn
+      |> json(%{message: "User deleted."})
+    else
+      conn
+      |> json(%{message: "You can't modify"})
     end
+  end
+
+  def get_current_userid(conn, _) do
+    current_user = Accounts.current_user(conn)
+    conn |> json(%{message: current_user.id})
   end
 
 end
--- doreiclient/lib/doreiclient_web/router.ex
+++ doreiclient/lib/doreiclient_web/router.ex
@@ -5,24 +5,35 @@ defmodule DoreiclientWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :auth do
+    plug Doreiclient.Accounts.Pipeline
+  end
+
+  pipeline :ensure_auth do
+    plug Guardian.Plug.EnsureAuthenticated
+  end
+
   scope "/", DoreiclientWeb do
-    pipe_through :api
+    pipe_through [:api, :auth]
 
     post "/signin", UserController, :create
-    post "/deleteacc", UserController, :delet
     post "/login", SessionController, :login
     post "/logout", SessionController, :logout
     post "/refresh_token", SessionController, :refresh_token
-
     get "/", PageController, :index
-    resources "/users", UserController, except: [:new, :edit] # post: ユーザー追加
+  end
 
+  scope "/", DoreiclientWeb do
+    pipe_through [:api, :auth, :ensure_auth]
+    resources "/users", UserController, except: [:new, :edit] # post: ユーザー追加
+    delete "/deleteacc", UserController, :delete
     resources "/tasks", TaskController, except: [:new, :edit]
     get "/taskdata", TaskController, :get_task_data
     get "/taskdata/timedifference", TaskController, :time_difference
     post "/changeworker", TaskController, :changeworker
     post "/setdeadline", TaskController, :set_deadline
     post "/accomp", TaskController, :accomp
+    get "/current_userid", UserController, :get_current_userid
   end
 
   # Other scopes may use custom stacks.
